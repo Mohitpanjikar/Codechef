1a066f23ed0a81038fd8ae8a9fb3da1fc33f09aa
Mohit Panjikar <98936526+Mohitpanjikar@users.noreply.github.com>
2022-07-03 23:08:23 +0530

    Add files via upload

---
+++ ENSPACE .cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x,y;
+        cin>>n>>x>>y;
+        if(n>=(x*1+y*2))
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;
+    }
+	return 0;
+}
