f2a672f15688da97f22d4566eee5d3d9ecdd67dc
Mohit Panjikar <98936526+Mohitpanjikar@users.noreply.github.com>
2023-03-22 22:15:16 +0530

    Add files via upload

---
+++ CANDIVIDE.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x%3 == 0)
+	    cout<<"Yes"<<endl;
+	    else
+	    cout<<"No"<<endl;
+	}
+	return 0;
+}
